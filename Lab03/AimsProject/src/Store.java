dcc82005a635187469956fee6458e470eb549f69
Tran Van Toan <Toan.TV214932@sis.hust.edu.vn>
2023-05-30 20:56:57 +0700

    topic/store commit

---
+++ Lab03/AimsProject/src/Store.java
@@ -0,0 +1,30 @@
+
+import java.util.*;
+
+public class Store {
+	private DigitalVideoDisc itemsInStore[] = new DigitalVideoDisc[0];
+	
+	public void addDVD(DigitalVideoDisc disc) {
+		List<DigitalVideoDisc> itemsInStoreList = 
+				new ArrayList<DigitalVideoDisc>(Arrays.asList(itemsInStore));
+		
+		if (itemsInStoreList.contains(disc)) {
+			System.out.println("The disc has already been added");
+		}
+		else {
+			itemsInStoreList.add(disc);
+			itemsInStore = itemsInStoreList.toArray(itemsInStore);
+			System.out.println("Disc added successfully");
+		}
+	}
+	
+	public void removeDVD(DigitalVideoDisc disc) {
+		List<DigitalVideoDisc> itemsInStoreList = 
+				new ArrayList<DigitalVideoDisc>(Arrays.asList(itemsInStore));
+		itemsInStoreList.remove(disc);
+		itemsInStore = new DigitalVideoDisc[itemsInStoreList.size()];
+		itemsInStore = itemsInStoreList.toArray(itemsInStore);		
+		System.out.println("Disc removed successfully");	
+	}
+}
+
